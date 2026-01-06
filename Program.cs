9ce8a18fe0a5000c064017af3bb0927c5e5b2d20
Ruan <ruanmeyer238@gmail.com>
2026-01-06 15:26:49 +0200

    Local Host

--- Program.cs
+++ Program.cs
@@ -8,12 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
 builder.Services.AddControllersWithViews();
 
-// Configure MySQL Database
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
-// Configure Identity
 builder.Services.AddIdentity<User, IdentityRole>(options =>
 {
     options.Password.RequireDigit = true;
@@ -25,7 +23,6 @@ builder.Services.AddIdentity<User, IdentityRole>(options =>
 .AddEntityFrameworkStores<ApplicationDbContext>()
 .AddDefaultTokenProviders();
 
-// Configure authentication
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/Account/Login";
@@ -37,7 +34,6 @@ builder.Services.AddSession();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -56,7 +52,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Dashboard}/{id?}");
 
-// Initialize database with seed data
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
@@ -66,13 +61,10 @@ using (var scope = app.Services.CreateScope())
 
     try
     {
-        // Ensure database is created
         context.Database.EnsureCreated();
 
-        // Seed existing data
         DbInitializer.Initialize(context);
 
-        // Seed default supervisor user
         var supervisorEmail = "supervisor@g.com";
         var existingUser = await userManager.FindByEmailAsync(supervisorEmail);
 
